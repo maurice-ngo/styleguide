39dc2e5e4bfb767b2f43a0cd0da9ddd40c7a4771
Ryan Atkinson <ryana804@gmail.com>
2016-09-23 11:39:08 -0700

    got slick-carousel working and looking good

--- src/assets/lib/lib.js
+++ src/assets/lib/lib.js
@@ -1,5 +1,14 @@
 require('./lib.scss')
+require('slick-carousel')
 
-// CODE REVIEW
-// is this the proper way?
-window.jQuery = window.$ = require('jquery')
+window.jQuery = window.$ = require('jquery');
+
+$('document').ready(function () {
+  $('.product-image-carousel').slick({
+    lazyLoad: 'progressive',
+    // lazyLoad: 'ondemand', // could save bandwidth, but seems buggy
+    dots: true,
+    dotsClass: 'product-image-carousel__dots',
+    arrows: false
+  });
+});
