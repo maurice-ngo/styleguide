e927605b7dc7d69e079fda5df4536f5d89e52035
Ryan Atkinson <ryana804@gmail.com>
2016-10-04 22:43:51 -0700

    moved product color dropdown's onchange event out of the html

--- src/assets/modules/modules.js
+++ src/assets/modules/modules.js
@@ -5,7 +5,9 @@ import './accordion';
 require('./accordion')
 require('./estimated-delivery')
 require('./size-dropdown-color')
+require('./product-color-change')
 
 $(document).ready(() => {
   $('.js-accordion').accordion();
+  $('.product-option--color .product-option__select').colorChange();
 });
