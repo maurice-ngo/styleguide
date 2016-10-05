267ad7270bd36a9cdfcc23abe110e407375f98c9
Ryan Atkinson <ryana804@gmail.com>
2016-10-05 10:37:10 -0700

    converted product-delivery js to babel style

--- src/assets/modules/modules.js
+++ src/assets/modules/modules.js
@@ -3,11 +3,12 @@ import './modules.scss';
 import './accordion';
 
 require('./accordion')
-require('./product-delivery')
+require('./product-delivery-update')
 require('./product-size-style')
 require('./product-color-change')
 
 $(document).ready(() => {
   $('.js-accordion').accordion();
+  $('.product-delivery').deliveryUpdate();
   $('.product-option--color .product-option__select').colorChange();
 });
