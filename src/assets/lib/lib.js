1e09a45249ef348bbad5a01fd7df62cbcbd453fd
Ryan Atkinson <ryana804@gmail.com>
2016-10-05 21:07:00 -0700

    added badges, updated image carousel

--- src/assets/lib/lib.js
+++ src/assets/lib/lib.js
@@ -4,11 +4,12 @@ require('slick-carousel')
 window.jQuery = window.$ = require('jquery');
 
 $('document').ready(function () {
-  $('.product-image-carousel').slick({
+  $('.product-image-carousel__imgs').slick({
     lazyLoad: 'progressive',
     // lazyLoad: 'ondemand', // could save bandwidth, but seems buggy
     dots: true,
     dotsClass: 'product-image-carousel__dots',
+    appendDots: '.product-image-carousel',
     arrows: false
   });
 });
