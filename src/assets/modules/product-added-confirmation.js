8ca07c3aa0dc88f445d8cbc06cb47df3fb84ab6b
Ryan Atkinson <ryana804@gmail.com>
2016-10-12 13:36:09 -0700

    updated js functions with intention of making them more readable and testable

--- src/assets/modules/product-added-confirmation.js
+++ src/assets/modules/product-added-confirmation.js
@@ -1,71 +1,77 @@
+/**
+ * Shows 'product added confirmation' modal when 'add to bag' is clicked
+ * @module addedConfirmation
+ */
+
 import $ from 'jquery';
+import registerJQueryPlugin from '../lib/register-jquery-plugin';
 
-// Settings
-export const pluginName = 'addedConfirmation';
+// Expose the function as a jQuery plugin for ease of use
+export const PLUGIN_NAME = 'addedConfirmation';
+registerJQueryPlugin(PLUGIN_NAME, addedConfirmation);
 
-// In case there are ever defaults
-export const DEFAULTS = {
-  'template': 'product-added-confirmation'
+export const TEMPLATE_ID = 'product-added-confirmation';
+export const TARGET_WRAP_ID = 'content';
+export const CONCEAL_CLASS = 'u-conceal';
+
+/**
+ * Initializes color dropdown changes.
+ * @param {HTMLElement} el - The select dropdown we're attaching to
+ */
+export default function addedConfirmation(el) {
+    const modal = createModal();
+    attachSubmitListener(el, modal);
 };
 
-export default class Plugin {
-  constructor(element, options) {
-    this.element = element;
-    this.options = $.extend( {}, DEFAULTS, options) ;
+/**
+ * Creates & attaches modal.
+ * @return {HTMLElement} Immutable modal element
+ */
+const createModal = () => {
+  // create the modal now, so we don't recreate on every submit
+  let modal = document.createElement('DIV');
 
-    this.init();
-  }
+  modal.className = 'modal ' + CONCEAL_CLASS;
+  modal.innerHTML = document.getElementById(TEMPLATE_ID).innerHTML;
+  // to override the default modal display: none
+  modal.style.display = 'block';
 
-  init() {
-    // cache this.options.template id
-    var template = this.options.template;
-    var CONCEAL_CLASS = 'u-conceal';
+  attachClickListener($(modal));
 
-    // create the modal now, so we don't recreate on every submit
-    var modal = document.createElement('DIV');
-    // to override the default modal display: none
-    modal.style.display = 'block';
-    // hide using visibility, for transform
-    modal.className = 'modal ' + CONCEAL_CLASS;
-    modal.innerHTML = document.getElementById(template).innerHTML;
-
-    // add modal once
-    document.getElementById('content').appendChild(modal);
-    // (unfortunately, waiting to append this when needed [in the form submit below] ruins our transition effect)
-
-    // when the form submits, show the modal
-    var form = $(this.element).closest('form');
-
-    form.submit(function(e) {
-      e.preventDefault();
-
-      // create jQuery object from modal
-      var confirmation = $(modal);
-
-      // reveal confirmation modal
-      confirmation.removeClass(CONCEAL_CLASS);
-
-      // set up the click listener to close confirmation modal
-      confirmation.click(function(e) {
-        var target = $(e.target);
-
-        // conceal if click is on the background or the 'continue' link
-        if (target.hasClass('modal') || target.hasClass('modal__continue')) {
-          e.preventDefault();
-          // conceal modal again
-          confirmation.addClass(CONCEAL_CLASS);
-        }
-      });
-    });
-  }
+  return document.getElementById(TARGET_WRAP_ID).appendChild(modal);
+  // (unfortunately, waiting to append this when needed [in the form submit below] ruins our transition effect)
 };
 
-// Plugin wrapped ctor
-$.fn[pluginName] = function ( options ) {
-  return this.each(function () {
-    if (!$.data(this, 'plugin_' + pluginName)) {
-      $.data(this, 'plugin_' + pluginName,
-      new Plugin( this, options ));
+/**
+ * Click listener to close confirmation modal
+ * @param {jQuery} el - The confirmation modal to show
+ */
+const attachClickListener = (el) => {
+  el.click(evt => {
+    let target = $(evt.toElement);
+
+    // conceal if click is on the background or the 'continue' link
+    if (target.hasClass('modal') || target.hasClass('modal__continue')) {
+      evt.preventDefault();
+      // conceal modal again
+      el.addClass(CONCEAL_CLASS);
     }
+  });
+}
+
+/**
+ * Adds 'submit' listener to parent 'form'.
+ * @param {HTMLElement} target - The element that was clicked, inside the form
+ * @param {HTMLElement} confirmation - The confirmation modal to show
+ */
+const attachSubmitListener = (target, confirmation) => {
+  // when the form submits, show the modal
+  let form = $(target).closest('form');
+
+  form.submit(function(e) {
+    e.preventDefault();
+
+    // reveal confirmation modal
+    $(confirmation).removeClass(CONCEAL_CLASS);
   });
 };
