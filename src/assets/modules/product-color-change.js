c07b1a10a05f256ee7325c06e3ccb9fd287a20f6
Geoff Berger <geoffberger@gmail.com>
2016-10-12 22:24:27 -0600

    added options and small updates to product-color-change

--- src/assets/modules/product-color-change.js
+++ src/assets/modules/product-color-change.js
@@ -10,26 +10,32 @@ import registerJQueryPlugin from '../lib/register-jquery-plugin';
 export const PLUGIN_NAME = 'colorChange';
 registerJQueryPlugin(PLUGIN_NAME, colorChange);
 
+const DEFAULT_OPTIONS = {
+  redirectFunc: val => document.location.href = val,
+};
+
 /**
  * Initializes color dropdown changes.
  * @param {HTMLElement} el - The select dropdown we're attaching to
  */
-export default function colorChange(el) {
+export default function colorChange(el, options = {}) {
+  const ops = Object.assign({}, DEFAULT_OPTIONS, options);
   // run whenever size dropdown changes
-  attachChangeListener($(el));
+  attachChangeListener($(el), options);
 };
 
 /**
  * Attaches change event to select dropdown.
  * @param {HTMLElement} dropdown - The select dropdown
  */
-const attachChangeListener = (dropdown) => {
-  dropdown.change(({currentTarget}) => {
+const attachChangeListener = (dropdown, { redirectFunc }) => {
+  dropdown.change(({ currentTarget }) => {
     // get selected after every change
     let selected = currentTarget.options[currentTarget.selectedIndex];
 
     // change location.href
-    if (selected.value)
-      document.location.href = selected.value;
+    if (selected.value) {
+      redirectFunc(selected.value);
+    }
   })
 };
