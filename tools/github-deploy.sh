e3201b2122ec676786f607e115ea824ab871c4be
Geoff Berger <geoffberger@gmail.com>
2016-10-17 21:07:00 -0600

    quickly threw together deploy script to help automate things because I am weird like that

---
+++ tools/github-deploy.sh
@@ -0,0 +1,20 @@
+project=$1
+
+if [ -z $project ]; then
+  echo "github project name is required but is empty, later"
+  exit -1
+fi
+
+project_dir="repos/$project"
+cwd=`pwd`
+
+rm -rf repos
+git clone git@github.com:FiresqueakLLC/$project.git $project_dir
+cp -r lib site $project_dir
+
+cd $project_dir
+git add .
+git commit -m 'updates to lib and site'
+git push origin master
+
+cd $cwd
