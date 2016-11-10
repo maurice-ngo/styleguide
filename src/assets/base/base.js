2febb8bcc768042d79468ade7b5e1b5ac2cffcfb
ryan atkinson <ryana804@gmail.com>
2016-11-10 10:00:55 -0800

    added load-images module
    using here for deferred loading of images below the fold, but the
    module can be used whenever by passing in a container and optional
    selector & attribute name

--- src/assets/base/base.js
+++ src/assets/base/base.js
@@ -1,7 +1,12 @@
 import $ from 'jquery';
 
 import './base.scss';
+import loadImages from './load-images';
 
 $(document).ready(() => {
   $('body').attr('ontouchstart', '');
+});
+
+$(window).load(() => {
+  $('body').loadImages();
 });
