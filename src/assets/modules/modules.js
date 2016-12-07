267a38a28d737126ccab3f824157e9cbf0b36c9e
ryan atkinson <ryana804@gmail.com>
2016-12-06 21:06:39 -0800

    removed unnecessary DOM references for .product

--- src/assets/modules/modules.js
+++ src/assets/modules/modules.js
@@ -9,16 +9,19 @@ import './product-color-change';
 import './product-oos';
 
 $(document).ready(() => {
-  $('.product .product__image--carousel').slick({
+  $('.js-accordion').accordion();
+
+  const $product = $('.product');
+
+  $product.find('.product__image--carousel').slick({
     lazyLoad: 'progressive',
     dots: true,
     dotsClass: 'product__image--carousel__dots',
     appendDots: '.product__image',
     arrows: false
   });
-  $('.js-accordion').accordion();
-  $('.product .product__option--size .product__option-select').sizeChange();
-  $('.product .product__option--color .product__option-select').colorChange();
-  $('.product .product__cta button[type="submit"]').addToBag();
-  $('.product .product-option--oos').oosProduct();
+  $product.find('product__option--size .product__option-select').sizeChange();
+  $product.find('product__option--color .product__option-select').colorChange();
+  $product.find('product__cta button[type="submit"]').addToBag();
+  $product.find('product-option--oos').oosProduct({ $wrap: $product });
 });
