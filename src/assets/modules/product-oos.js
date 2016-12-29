4edd22589b0ec491d23e3c7368c0d940e11a17ee
Geoff Berger <geoffberger@gmail.com>
2016-12-29 12:13:33 -0800

    setup product-oos to take into consideration constants which will be updated later on

--- src/assets/modules/product-oos.js
+++ src/assets/modules/product-oos.js
@@ -4,26 +4,40 @@
  */
 
 import $ from 'jquery';
+import result from 'lodash/result';
 
 import registerJQueryPlugin from '../lib/register-jquery-plugin';
 import updateCTA from './product-cta-update';
 import updateDeliveryDate from './delivery-date-update';
-import { PRICE_ATTR, REGULAR_PRICE_ATTR } from '../modules/product-price-update';
+
+// TODO: move this over to pdp constants
+export const PRICE_ATTR = 'price';
+export const REGULAR_PRICE_ATTR = 'regular-price';
+export const WRAP_CLASS = 'product';
 
 // Expose the function as a jQuery plugin for ease of use
 export const PLUGIN_NAME = 'oosProduct';
 registerJQueryPlugin(PLUGIN_NAME, oosProduct);
 
-const WRAP_CLASS = 'product';
+const PRODUCT_OOS_OPTIONS = {
+  $wrap: el => $(el).closest(`.${WRAP_CLASS}`),
+  updateDeliveryDate,
+  updateCTA,
+};
 
 /**
  * Initializes changes for oos product.
+ * @see PRODUCT_OOS_OPTIONS
  * @param {HTMLElement} el - The select dropdown we're attaching to
+ * @param {Object} [options={}] - Additional configuration
  */
 export default function oosProduct(el, options = {}) {
-  const $wrap = options.$wrap || $(el).closest(`.${WRAP_CLASS}`);
+  const opts = Object.assign({}, PRODUCT_OOS_OPTIONS, options);
+  const { updateDeliveryDate, updateCTA } = opts;
+  const wrap = result(opts, '$wrap')[0];
+
   const data = {
-    wrap: $wrap[0],
+    wrap,
     wrapBlockClass: WRAP_CLASS,
     chosen: {
       oos: true,
@@ -41,11 +55,11 @@ export default function oosProduct(el, options = {}) {
  * @return {Boolean} Whether price is less than regualarPrice
  */
 const isOnSale = (item) => {
-  const price = item.getAttribute(PRICE_ATTR);
-  const regular = item.getAttribute(REGULAR_PRICE_ATTR);
+  const price = item.getAttribute(`data-${PRICE_ATTR}`);
+  const regular = item.getAttribute(`data-${REGULAR_PRICE_ATTR}`);
 
   if (!price || !regular)
     return false;
 
-  return parseFloat(price) < parseFloat(regularPrice);
+  return parseFloat(price) < parseFloat(regular);
 };
