2a870d8045169fb471349f66e03d1b434f9d81d6
ryan atkinson <ryana804@gmail.com>
2017-01-09 16:42:14 -0800

    updated toggleHide to accept $field
    RE-133
    Instead of accepting an inputElement, and traversing the DOM for the closest ".field" wrapper, we instead take $field as an argument. This has the added benefit of allowing us to disable all inputs and selects within the $field.

--- src/assets/elements/fields/toggle-hidden.js
+++ src/assets/elements/fields/toggle-hidden.js
@@ -7,24 +7,18 @@ export const DEFAULT_OPTIONS = {
 
 /**
  * Toggle whether Field is Hidden
- * Also toggles disabled property of the form element,
+ * Also toggles disabled property of contained form elements,
  * so hidden fields are not validated by the browser
- * @param {HTMLElement} element - Form element
+ * @param {jQueryElement} $field - Form wrapper
  * @param {boolean} shouldHide - Whether the email field should be hidden
  * @param {object} options - List of options
  * @return {jQueryElement} Field wrapper
  */
-export default function toggleHidden(element, shouldHide, options = {}) {
+export default function toggleHidden($field, shouldHide, options = {}) {
   const { fieldClassName, hiddenModifier } = Object.assign({}, DEFAULT_OPTIONS, options);
-  const $element = $(element);
-  const $field = $element.closest(`.${fieldClassName}`);
 
-  if (!$field.length) {
-    throw new Error('Field not found by toggleHidden()');
-  }
-
-  // set :disabled to avoid browser form validation
-  $element.prop('disabled', shouldHide);
+  // set inputs to :disabled to avoid native browser form validation
+  $field.find('input, select').prop('disabled', shouldHide);
   $field.toggleClass(`${fieldClassName}--${hiddenModifier}`, shouldHide);
 
   return $field;
--- src/assets/elements/fields/toggle-hidden.spec.js
+++ src/assets/elements/fields/toggle-hidden.spec.js
@@ -5,13 +5,15 @@ import chaiJquery from 'chai-jquery';
 import input from '../../../partials/input-text.html';
 import toggleHidden, { DEFAULT_OPTIONS } from './toggle-hidden'
 
+chai.use(chaiJquery);
+
 describe('Toggle Hidden', () => {
   const { fieldClassName, hiddenModifier } = DEFAULT_OPTIONS;
   const hiddenClassName = `${fieldClassName}--${hiddenModifier}`;
   const ELEMENT_ID = 'email';
   let $form;
+  let $field;
   let $inputElement;
-  let inputElement;
 
   beforeEach( () => {
     $form = $(fixture.set(`
@@ -20,25 +22,25 @@ describe('Toggle Hidden', () => {
       </form>
     `));
     $inputElement = $form.find(`#${ELEMENT_ID}`);
-    inputElement = $inputElement[0];
+    $field = $inputElement.closest('.field');
   });
 
   afterEach(() => fixture.cleanup());
 
   it('should find a field wrapper', () => {
-    expect($inputElement.closest('.field')).to.have.length.above(0);
+    expect($field).to.have.length.above(0);
   });
 
   describe('When form element should be hidden', () => {
     const shouldHide = true;
 
     it('should disable the form element', () => {
-      toggleHidden(inputElement, shouldHide);
+      toggleHidden($field, shouldHide);
       expect($inputElement).to.be.disabled;
     });
 
     it('should hide the field wrapper', () => {
-      const $field = toggleHidden(inputElement, shouldHide);
+      toggleHidden($field, shouldHide);
       expect($field).to.have.class(hiddenClassName);
     });
   });
@@ -47,12 +49,12 @@ describe('Toggle Hidden', () => {
     const shouldHide = false;
 
     it('should not disable the form element', () => {
-      toggleHidden(inputElement, shouldHide);
+      toggleHidden($field, shouldHide);
       expect($inputElement).not.to.be.disabled;
     });
 
     it('should not hide the field wrapper', () => {
-      const $field = toggleHidden(inputElement, shouldHide);
+      toggleHidden($field, shouldHide);
       expect($field).to.not.have.class(hiddenClassName);
     });
   });
